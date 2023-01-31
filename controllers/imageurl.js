f513924861a2888a4cfc2fdb9a57fc1f8675220a
wpellas <william.pellas@hotmail.com>
2023-01-31 09:24:05 +0100

    0.1.0

---
+++ controllers/imageurl.js
@@ -0,0 +1,39 @@
+const handleApiCall = (req, res) => {
+    const raw = JSON.stringify({
+    user_app_id: {
+      user_id: 'wpellas',
+      app_id: 'my-first-application'
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: req.body.input
+          }
+        }
+      }
+    ]
+    });
+    
+    const requestOptions = {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        Authorization: 'Key 0fcde0454b5442ff8779ee309bedf2b2'
+      },
+      body: raw
+    };
+    fetch(
+      'https://api.clarifai.com/v2/models/face-detection/versions/45fb9a671625463fa646c3523a3087d5/outputs',
+      requestOptions
+    )
+    .then(response => response.json())
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to workn with API'))
+}
+
+module.exports = {
+    handleApiCall: handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+//////////// SEPARATION OF CONCERNS
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const imageurl = require('./controllers/imageurl')
+
+//////////// KNEX DB
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'loke',
+    database : 'smartbrain'
+  }
+});
+
+//////////// EXPRESS & CORS
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//////////// TEMP DATABASE
+/*/ const database = {
+//     users: [
+//         {
+//             id: '123',
+//             name: 'Loke',
+//             password: "snackies",
+//             email: 'loke@keasimp.se',
+//             entries: 0,
+//             joined: new Date()
+//         },{
+//             id: '124',
+//             name: 'Kea',
+//             password: "lokecute",
+//             email: 'missindependent@gmail.com',
+//             entries: 0,
+//             joined: new Date()
+//         },
+//     ],
+//     login: [
+//         {
+//             id: '987',
+//             hash: '',
+//             email: 'loke@keasimp.se'
+//         }
+//     ]
+// }
+///////////*/
+
+//-------------------------------------ENDPOINTS-------------------------------------///
+
+//////////// HOME DATABASE SELECT
+app.get('/', (req, res) => { res.send(db.users) })
+//////////// SIGNIN
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+//////////// REGISTER
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+//////////// USER ID
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+//////////// RANK INCREMENT
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+//////////// API
+app.post('/imageurl', (req, res) => { imageurl.handleApiCall(req, res)})
+
+//////////// HOST
+const DATABASE_URL = process.env.DATABASE_URL
+app.listen(3000, () => {
+    console.log(`Server is listening on port ${DATABASE_URL}.`);
+});
